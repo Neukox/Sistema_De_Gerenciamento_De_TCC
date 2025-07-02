218d191cc5fae327eb8d076d744f03e82b954e17
GabrielF0900 <gabrielcfonline0900@gmail.com>
2025-07-02 15:23:44 -0300

    Enviando atualizações do Frontend

--- Sistema_De_Gerenciamento_De_TCC-master/Frontend/src/main.tsx
+++ Sistema_De_Gerenciamento_De_TCC-master/Frontend/src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import App from './App'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import Login from './pages/Login/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
   </StrictMode>
 )
