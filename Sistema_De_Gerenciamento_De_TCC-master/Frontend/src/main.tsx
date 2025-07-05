7064e7238d63934fb18a78d876fdda06429c2ad6
GabrielF0900 <gabrielcfonline0900@gmail.com>
2025-07-05 10:19:37 -0300

    Enviando atualizações sobre Backend

--- Sistema_De_Gerenciamento_De_TCC-master/Frontend/src/main.tsx
+++ Sistema_De_Gerenciamento_De_TCC-master/Frontend/src/main.tsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import Login from './pages/Login/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Login />
     </BrowserRouter>
   </StrictMode>
 )
