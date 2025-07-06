6bcdba55647d3ca7970743f0b9202f652749dfb1
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-07-06 12:41:31 -0300

    Adicionando rotas de autenticação e centralizando as rotas da API

---
+++ Backend/src/api/routes/index.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import authRoutes from "./authRoutes";
+
+// Arquivo de centralização das rotas da API
+// Este arquivo importa e agrupa todas as rotas da API.
+
+const routes = Router();
+
+// Usando as rotas de autenticação
+routes.use("/", authRoutes);
+
+// Exporta as rotas
+export default routes;
--- Sistema_De_Gerenciamento_De_TCC-master/Backend/src/server.ts
+++ Sistema_De_Gerenciamento_De_TCC-master/Backend/src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import routes from './minhaAPI/Routes/routes'; // ajuste o caminho se necessário
+import express from "express";
+//import routes from "./api/routes/";
+// import { errorHandler } from "./api/middlewares/errorHandler";
+import routes from "./minhaAPI/Routes/routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +13,9 @@ app.use(express.json());
 
 // Usar rotas externas
 app.use(routes);
+
+// Middleware de tratamento de erros
+// app.use(errorHandler);
 
 // Iniciar servidor
 app.listen(PORT, () => {
