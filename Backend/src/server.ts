7f614dfdf47dec40afb03a750862a6dad1e4ab0a
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-07-08 13:07:30 -0300

    Recuperando arquivos do backend

---
+++ Backend/src/server.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import routes from "./api/routes";
+import { errorHandler } from './api/middlewares/errorHandler';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para habilitar CORS
+app.use(cors());
+
+// Middleware para entender JSON
+app.use(express.json());
+
+// Usar rotas externas
+app.use(routes);
+
+// Middleware de tratamento de erros
+app.use(errorHandler);
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT} 🚀`);
+});
