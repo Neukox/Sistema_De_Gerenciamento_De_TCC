89cd345f9bf1f58883b5d4773e2e755c87872edd
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-07-08 11:37:28 -0300

    Adiciona rotas para TCCs

--- Backend/src/api/routes/TCCRoutes.ts
+++ Backend/src/api/routes/TCCRoutes.ts
@@ -2,8 +2,16 @@ import Router from "express";
 import createTCCController from "../controllers/TCC/createController";
 import authorization from "../middlewares/authorization";
 import allowRoles from "../middlewares/allowRoles";
+import getTccByIdController from "../controllers/TCC/getByIdController";
+import getAllController from "../controllers/TCC/getAllController";
 
 const TCCRoutes = Router();
+
+// Rota para obter todos os TCCs
+TCCRoutes.get("/", [authorization], getAllController);
+
+// Rota para obter um TCC específico
+TCCRoutes.get("/:id", [authorization], getTccByIdController);
 
 // Rota para criar um novo TCC
 TCCRoutes.post(
--- Backend/src/api/routes/index.ts
+++ Backend/src/api/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRoutes from "./authRoutes";
 import alunosRoutes from "./alunosRoutes";
 import professorRoutes from "./professorRoutes";
+import TCCRoutes from "./TCCRoutes";
 
 // Arquivo de centralização das rotas da API
 // Este arquivo importa e agrupa todas as rotas da API.
@@ -14,6 +15,8 @@ routes.use("/", authRoutes);
 routes.use("/alunos", alunosRoutes);
 // Usando as rotas de professores
 routes.use("/professores", professorRoutes);
+// Usando as rotas de TCC
+routes.use("/tccs", TCCRoutes);
 
 // Exporta as rotas
 export default routes;
