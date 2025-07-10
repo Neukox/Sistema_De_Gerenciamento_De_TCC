718c31def1d4688a1f75250d4f7308a28fb3e16d
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-07-10 10:34:37 -0300

    refatora componente Button

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,10 +1,16 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0F2C67", // Azul escuro
+        secondary: "#F3C50D", // Amarelo
+        neutral: "#F4E185", // Âmbar
+      },
+      fontFamily: {
+        OpenSans: ["Open Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
