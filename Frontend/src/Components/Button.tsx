db2f13287de43aec381623ba514e039c3ea374da
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-07-10 10:34:37 -0300

    refatora componente Button

--- Frontend/src/Components/Button.tsx
+++ Frontend/src/Components/Button.tsx
@@ -1,29 +1,55 @@
-type ButtonProps = {
-    type: void | 'button' | 'submit' | 'reset';
-    bgColor?: string;
-    width?: string;
-    height?: string;
-    children?: React.ReactNode;
-    disabled?: boolean;
-    onClick?: () => void;
-}
+import React from "react";
+import { cn } from "../utils/cn";
 
-function Button ({type = 'button', bgColor = 'bg-blue-500', children, width, height, disabled = false, onClick} : ButtonProps) {
-    return (
-          <button 
-            type={type}
-            disabled={disabled}
-            onClick={onClick}
-            className={`px-4 py-2 rounded-lg text-white font-semibold transition-transform duration-300 ${width} ${height} ${bgColor} ${
-              disabled 
-                ? 'opacity-50 cursor-not-allowed' 
-                : 'hover:opacity-95 hover:translate-y-1'
-            }`}
-          >
-            {children}
-          </button>
-          
-    );
+type Variants = {
+  default: string;
+  primary: string;
+  secondary: string;
+  neutral: string;
+};
+
+// Define as variantes de estilo para o botão
+const variants: Variants = {
+  default: "text-black border border-gray-300 hover:bg-gray-100",
+  primary: "bg-primary hover:bg-primary/80 text-white",
+  secondary: "bg-secondary hover:bg-secondary/80 text-white",
+  neutral: "bg-neutral hover:bg-neutral/80 text-white",
+};
+
+type ButtonProps = {
+  variant?: keyof Variants;
+  className?: string;
+  children?: React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+/**
+ * Componente de Botão Personalizado.
+ * Este componente permite a criação de botões com diferentes variantes de estilo.
+ * @param {keyof Variants} [variant] - Define o estilo do botão. Opções: "default", "primary", "secondary", "neutral".
+ * @param {string} [className] - Classe CSS adicional para personalização.
+ * @param {React.ReactNode} [children] - Conteúdo do botão, geralmente texto ou ícones.
+ * @param {React.ButtonHTMLAttributes<HTMLButtonElement>} props - Outras propriedades HTML do botão, como `onClick`, `disabled`, etc.
+ * @return {JSX.Element} Um botão estilizado com base na variante selecionada.
+ */
+
+function Button({
+  variant = "default",
+  className,
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        "px-4 py-2 rounded-lg text-black font-semibold hover:translate-y-1 transition-transform duration-300",
+        variants[variant],
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
