8adbae60c6e80c9c550aea5d49e0b025261c00ef
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-07-10 17:40:48 -0300

    adiciona componente ToastContainer para exibição de notificações

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        theme="colored"
+      />
     </BrowserRouter>
   </StrictMode>
 );
