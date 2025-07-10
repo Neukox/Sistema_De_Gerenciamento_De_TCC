6167814816e77c215c1065716c0b4557898e4e4b
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-07-10 15:48:55 -0300

    adiciona componente FormError para exibição de mensagens de erro em formulários

---
+++ Frontend/src/components/ui/form/FormError.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/utils/cn";
+import React from "react";
+
+type FormErrorProps = {
+  children?: React.ReactNode;
+} & React.HTMLAttributes<HTMLParagraphElement>;
+
+/**
+ * Componente de Erro de Formulário.
+ * Exibe mensagens de erro em formulários com estilo personalizado.
+ * @param {React.ReactNode} [children] - Conteúdo do erro, geralmente uma mensagem de erro.
+ * @param {React.HTMLAttributes<HTMLParagraphElement>} props - Outras propriedades HTML do parágrafo, como `className`, `style`, etc.
+ * @return {JSX.Element} Um parágrafo estilizado para exibir mensagens de erro em formulários.
+ */
+
+export default function FormError({ children, ...props }: FormErrorProps) {
+  return (
+    <p className={cn("text-red-500 text-sm mt-1", props.className)} {...props}>
+      {children}
+    </p>
+  );
+}
--- Frontend/src/components/ui/form/index.ts
+++ Frontend/src/components/ui/form/index.ts
@@ -1,6 +1,7 @@
 import Button from "../Button";
 import InputPassword from "../shared/form/InputPassword";
+import FormError from "./FormError";
 import Input from "./Input";
 import Label from "./Label";
 
-export { Input, InputPassword, Label, Button };
+export { Input, InputPassword, Label, Button, FormError };
