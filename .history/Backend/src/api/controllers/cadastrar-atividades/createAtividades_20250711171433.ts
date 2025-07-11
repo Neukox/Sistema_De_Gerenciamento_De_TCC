f2504ead1e8507a539fa0edfcb0f7b5230821ebe
GabrielF0900 <gabrielcfonline0900@gmail.com>
2025-07-11 17:15:35 -0300

    Enviando estrutura base do algoritmo de criação de atividades.

---
+++ .history/Backend/src/api/controllers/cadastrar-atividades/createAtividades_20250711171433.ts
@@ -0,0 +1,7 @@
+//Algoritmo de criação de atividades apõs criar TCC.
+
+import { Request, Response } from "express";
+
+export async function createAtividades(req: Request, res: Response): Promise<void> {
+    
+}
