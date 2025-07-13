622765e19fdbe7b4e320db368d9c7e5d6c91a70d
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-07-13 17:26:38 -0300

    adiciona controlador e serviço para atualizar atividades

--- Backend/src/api/routes/atividadesRoutes.ts
+++ Backend/src/api/routes/atividadesRoutes.ts
@@ -2,6 +2,8 @@ import Router from "express";
 import createAtividadeController from "../controllers/atividades/createController";
 import allowRoles from "../middlewares/allowRoles";
 import authorization from "../middlewares/authorization";
+import updateAtividadeController from "../controllers/atividades/updateController";
+import validateNumberParams from "../middlewares/validateNumberParams";
 
 // Rotas para atividades
 
@@ -12,6 +14,13 @@ atividadesRouter.post(
   "/",
   [authorization, allowRoles(["ALUNO", "ADMIN"])],
   createAtividadeController
+);
+
+// Rota para atualizar uma atividade existente
+atividadesRouter.put(
+  "/:id",
+  [authorization, allowRoles(["ALUNO", "ADMIN"]), validateNumberParams],
+  updateAtividadeController
 );
 
 // Exportando o router
---
+++ Backend/src/api/services/atividade/updateService.ts
@@ -0,0 +1,34 @@
+import { $Enums, Atividade } from "@prisma/client";
+import {
+  getAtividadeById,
+  updateAtividade,
+} from "../../repositories/atividades/atividadesRepository";
+import { ResponseError } from "../../helpers/ResponseError";
+
+/**
+ * Serviço para atualizar uma atividade existente.
+ * @param id - ID da atividade a ser atualizada.
+ * @param data - Dados atualizados da atividade.
+ * @returns A atividade atualizada ou null se falhar.
+ */
+export default async function updateAtividadeService(
+  id: number,
+  data: Partial<Atividade>
+): Promise<Atividade | null> {
+  // Verifica se a atividade existe
+  const atividadeExistente = await getAtividadeById(id);
+
+  if (!atividadeExistente) {
+    throw new ResponseError(404, "Atividade não encontrada");
+  }
+
+  // converte o status para o formato correto, se fornecido
+  if (data.status) {
+    data.status = data.status.toUpperCase() as $Enums.StatusAtividade;
+  }
+
+  // Atualiza a atividade com os novos dados
+  const atividadeAtualizada = await updateAtividade(id, data);
+
+  return atividadeAtualizada;
+}
