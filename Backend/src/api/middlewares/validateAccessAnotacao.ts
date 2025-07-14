545f033e17430a7d27e4e28c4401682068e91700
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-07-14 16:02:30 -0300

    adiciona middlewares para validar o acesso a anotações e recursos de TCCs

---
+++ Backend/src/api/middlewares/validateAccessAnotacao.ts
@@ -0,0 +1,47 @@
+import { RequestWithUser } from "../types/auth";
+import { NextFunction, Response } from "express";
+import prisma from "../config/prisma";
+
+/**
+ * Middleware para validar o acesso a anotações de um TCC.
+ * Permite acesso se o usuário for o dono do TCC ou um administrador.
+ */
+
+export default async function validateAccessAnotacao(
+  req: RequestWithUser,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> {
+  const user = req.user;
+
+  // Verifica se o usuário é um administrador
+  if (user?.role === "ADMIN") {
+    return next();
+  }
+
+  const atividadeId = req.params.id;
+
+  // Busca a atividade pelo ID
+  const atividade = await prisma.atividade.findUnique({
+    where: { id: Number(atividadeId) },
+    include: { TCC: true },
+  });
+
+  if (!atividade) {
+    return res
+      .status(404)
+      .json({ message: "Atividade não encontrada.", success: false });
+  }
+
+  // Verifica se o usuário é o dono do TCC ou um administrador
+
+  if (atividade.TCC.Aluno_id === user?.id) {
+    return next();
+  }
+
+  return res.status(403).json({
+    message:
+      "Acesso negado. Você não tem permissão para acessar esta anotação.",
+    success: false,
+  });
+}
