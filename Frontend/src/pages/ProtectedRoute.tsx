6ab8cdee4f824546d64140a9d9fe083a8439dded
Israel Soares Porto <isaelporto15@gmail.com>
2025-07-21 19:07:47 -0300

    Adicionar proteção de rota em modo dev em ProtectedRoute.tsx

--- Frontend/src/pages/ProtectedRoute.tsx
+++ Frontend/src/pages/ProtectedRoute.tsx
@@ -14,11 +14,11 @@ type ProtectedRouteProps = {
 export default function ProtectedRoute({ roles }: ProtectedRouteProps) {
   const { user } = useAuth();
 
-  if (!user) {
+  if (!user && import.meta.VITE_DEV_PROTECT_ROUTE === "true") {
     return <Navigate to="/login" replace />;
   }
 
-  if (user && roles && !roles.includes(user.role)) {
+  if (user && roles && !roles.includes(user.role) && import.meta.VITE_DEV_PROTECT_ROUTE === "true") {
     return <Navigate to="/login" replace />;
   }
 
