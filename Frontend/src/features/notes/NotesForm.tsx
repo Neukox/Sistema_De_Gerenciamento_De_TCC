44ec0f54fbb7a5355de16c606ea6d212069dd361
Israel-SoaresPorto <isaelporto15@gmail.com>
2025-07-26 09:54:34 -0300

    Adicionar componente NotesForm para gerenciamento de anotações

---
+++ Frontend/src/features/notes/NotesForm.tsx
@@ -0,0 +1,55 @@
+import { FormError, Submit, Textarea } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { FaPlus } from "react-icons/fa";
+
+type NotesFormProps = {
+  onSubmit: (data: NoteFormData) => void;
+};
+
+type NoteFormData = {
+  nota: string;
+};
+
+export default function NotesForm({ onSubmit }: NotesFormProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<NoteFormData>({
+    defaultValues: {
+      nota: "",
+    },
+  });
+
+  const onSubmitForm = (data: NoteFormData) => {
+    onSubmit(data);
+    reset();
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <form
+        onSubmit={handleSubmit(onSubmitForm)}
+        className="w-full h-60 relative"
+      >
+        <Textarea
+          variant="primary"
+          placeholder="Adicione suas anotações..."
+          className="w-full h-full pb-10"
+          aria-invalid={!!errors.nota}
+          {...register("nota", {
+            required: "Anotação não pode ser vazia",
+          })}
+        />
+        <Submit
+          variant="primary"
+          className="flex items-center gap-2 absolute bottom-2 right-3 rounded-full"
+        >
+          <FaPlus className="size-4" />
+        </Submit>
+      </form>
+      {errors.nota && <FormError>{errors.nota.message}</FormError>}
+    </div>
+  );
+}
